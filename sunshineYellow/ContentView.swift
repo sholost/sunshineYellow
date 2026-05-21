4c7ebad91b1e1770d1fe212038c24265356d6f9c
sol richas <stoedirman@gmail.com>
2026-05-21 11:57:31 +0700

    Initial Commit

---
+++ sunshineYellow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sunshineYellow
+//
+//  Created by Sholihatul Richas on 21/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sunshineYellow/sunshineYellowApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunshineYellowApp.swift
+//  sunshineYellow
+//
+//  Created by Sholihatul Richas on 21/05/26.
+//
+
+import SwiftUI
+
+@main
+struct sunshineYellowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
